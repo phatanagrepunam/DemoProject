0feb49e92615b213491ef27aaf230bd3e581fbdb
phatangarepunam <phatangarepunam@gmail.com>
2025-11-11 15:22:57 +0530

    helloworld

---
+++ src/test/java/com/test/HelloWorld.java
@@ -0,0 +1,17 @@
+package com.test;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class HelloWorld {
+	@Test
+	 public void Hello() throws InterruptedException
+	 {
+		 System.out.println("Hello World");
+	 }
+}
